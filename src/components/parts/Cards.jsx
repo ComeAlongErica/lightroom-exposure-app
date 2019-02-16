b3f2854a86d572d9dbaf379b75a51f1119319031
ComeAlongErica <ericaraephoto@gmail.com>
2019-02-16 12:04:33 -0500

    card transitions on hover

--- src/components/parts/Cards.jsx
+++ src/components/parts/Cards.jsx
@@ -10,8 +10,9 @@ background-image: linear-gradient(-240deg, #332d49, #311439);
 margin: 10px;
 border-radius: 10px;
 box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
-transition: 0.3s cubic-bezier(0.68, -0.55, 0.27, 1.55);
+transition: transform 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55);
 :hover {
+  cursor: pointer;
   transform: scale(1.05);
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 }
