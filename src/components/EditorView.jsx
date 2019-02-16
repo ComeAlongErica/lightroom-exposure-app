29783d316d00290d03c22eb443347d9a0315911d
ComeAlongErica <ericaraephoto@gmail.com>
2019-02-16 17:33:34 -0500

    change view sizes

--- src/components/EditorView.jsx
+++ src/components/EditorView.jsx
@@ -2,11 +2,20 @@ import React, { Fragment } from 'react';
 import styled from 'styled-components'
 
 const EditorContainer = styled.div`
+flex-grow: 1;
 position: relative;
 background-color: #1d0c26;
 height: 95vh;
-width: 30vw;
 margin: 15px;
+`
+const SingleImg = styled.img`
+  width: 90%;
+  height: auto;
+  box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+  transition: transform .5s ease-out;
+  position: absolute;
+  top: 50%; left: 50%;
+  transform: translate(-50%,-50%);
 `
 const DoubleExposure = styled.img`
   width: 90%;
@@ -16,21 +25,17 @@ const DoubleExposure = styled.img`
   position: absolute;
   top: 50%; left: 50%;
   transform: translate(-50%,-50%);
-  mix-blend-mode: overlay;
+  mix-blend-mode: screen;
 `
 
 const EditorView = props => {
   const { showPicture } = props
+
   return (
     <EditorContainer>
-      <DoubleExposure src={showPicture.portraitSection} />
-
-      {Object.values(showPicture).map((img, index) => (
-        <Fragment key={index}>
-          <DoubleExposure src={img} />
-        </Fragment>
-      ))}
-
+      <SingleImg src={showPicture.portraitSection} />
+      <DoubleExposure src={showPicture.structureSection} />
+      <DoubleExposure src={showPicture.lightSection} />
     </EditorContainer >
   )
 }
