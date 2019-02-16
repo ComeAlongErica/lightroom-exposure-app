ca6b86e1f5b5a3f8743cec18bff8144f139f362e
ComeAlongErica <ericaraephoto@gmail.com>
2019-02-15 21:04:33 -0500

    created x for menu

--- src/components/SelectorView.jsx
+++ src/components/SelectorView.jsx
@@ -8,13 +8,14 @@ const theme = {
 };
 
 const SelectorContainer = styled.div`
+position: relative;
 display: flex;
 flex-direction: column;
 background-color: #1d0c26;
 height: 95vh;
 width: 30vw;
 margin: 15px;
-div {
+.cardContainer {
   flex-grow: 1;
   box-sizing: border-box;
   display: flex;
@@ -25,12 +26,50 @@ div {
   padding: 10px;
   height: 100%;
 }
+.fa-times {
+  font-size: 35px;
+  color: white;
+  margin: 20px;
+  position: absolute;
+  right: 0;
+  top: 0;
+  :hover {
+    cursor: pointer;
+  }
+}
+`
+
+const CloseX = styled.div`
+  position: absolute;
+  margin: 20px;
+  right: 0;
+  top: 0;
+  width: 32px;
+  height: 32px;
+  opacity: 0.3;
+:hover {
+  opacity: 1;
+}
+:before, :after {
+  position: absolute;
+  left: 15px;
+  content: '';
+  height: 33px;
+  width: 2px;
+  background-color: white;
+}
+:before {
+  transform: rotate(45deg);
+}
+:after {
+  transform: rotate(-45deg);
+}
 `
 
 const Title = styled.h1`
 font-size: 40px;
 color: white;
-margin: 20px 20px 10px 20px;
+margin: 15px 20px 10px 20px;
 `
 
 const SelectorView = () => {
@@ -47,19 +86,20 @@ const SelectorView = () => {
     {
       title: 'Provide Atmosphere',
       theme: 'linear-gradient(to right, #fcde8a, #ff8f89, #ff8f89, #ff8f89)',
-      description: 'Select the main portrait to edit'
+      description: 'Give the image depth'
     },
     {
       title: 'Add the Magic',
       theme: 'linear-gradient(to right, #42e697, #3cbabb, #3cbabb)',
-      description: 'Select lights, color, or grain'
+      description: 'This is where you add the splash of fun'
     }
   ]
 
   return (
     <SelectorContainer>
+      <CloseX />
       <Title>Build</Title>
-      <div>
+      <div className={'cardContainer'}>
         {editorCards.map(selector => (
           <Cards theme={{ main: selector.theme }} title={selector.title} description={selector.description} />
         ))}
